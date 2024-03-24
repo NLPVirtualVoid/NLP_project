eaacd6d195a21dffda9a58da4c2b1626625527e2
a-bayly <a.bayly@yahoo.com>
2024-03-24 16:44:26 +0000

    FilterDataset has new create dataset functionality

--- Test/human_eval/data.py
+++ Test/human_eval/data.py
@@ -7,7 +7,7 @@ import os
 ROOT = os.path.dirname(os.path.abspath(__file__))
 HUMAN_EVAL = os.path.join(ROOT, "..", "data", "HumanEval.jsonl.gz")
 HUMAN_EVAL = "C:/DEV/data/HumanEval/HumanEval.jsonl.gz"
-HUMAN_EVAL = "HumanEval.jsonl.gz"
+#HUMAN_EVAL = "HumanEval.jsonl.gz"
 
 
 def read_problems(evalset_file: str = HUMAN_EVAL) -> Dict[str, Dict]:
